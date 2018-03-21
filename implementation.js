d09c33fff4a36a02c3bd29fe240e0d330d5fb317
Jordan Harband <ljharb@gmail.com>
2018-03-21 22:34:10 +0000

    Add fallback implementation for pre-Symbol engines, for Arrays.

--- implementation.js
+++ implementation.js
@@ -6,8 +6,36 @@ var ThrowCompletion = function Throw(error) {
 	throw error;
 };
 
+var legacyAssign = function assign(obj, entries) {
+	for (var i = 0; i < entries.length; ++i) {
+		var entry = entries[i];
+		if (ES.Type(entry) !== 'Object') {
+			throw new TypeError('iterator returned a non-object; entry expected');
+		}
+
+		var key = ES.Get(entry, '0');
+		var value = ES.Get(entry, '1');
+		var propertyKey = ES.ToPropertyKey(key);
+		ES.CreateDataPropertyOrThrow(obj, propertyKey, value);
+	}
+};
+
+/* global Symbol */
+
+var hasSymbols = typeof Symbol === 'function' && typeof Symbol('foo') === 'symbol';
+
 module.exports = function fromEntries(iterable) {
 	var obj = {};
+
+	// this part isn't in the spec, it's for a reasonable fallback for pre-ES6 environments
+	if (!hasSymbols) {
+		if (!ES.IsArray(iterable)) {
+			throw new TypeError('this environment lacks native Symbols, and can not support non-Array iterables');
+		}
+		legacyAssign(obj, iterable);
+		return obj;
+	}
+
 	var iter = ES.GetIterator(iterable);
 
 	while (true) { // eslint-disable-line no-constant-condition
@@ -18,7 +46,7 @@ module.exports = function fromEntries(iterable) {
 
 		var nextItem = ES.IteratorValue(next);
 		if (ES.Type(nextItem) !== 'Object') {
-			var error = new TypeError('iterator returned a non-object');
+			var error = new TypeError('iterator returned a non-object; entry expected');
 			return ES.IteratorClose(iter, ThrowCompletion(error));
 		}
 
